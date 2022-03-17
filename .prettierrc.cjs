dfa6afd857de65561b7c75ac33cb3cb27486509f
fitgrace <fitingrace@gmail.com>
2022-03-17 17:50:28 +0800

    build: prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  // 是否在语句末尾打印分号
+  semi: false,
+
+  // 是否使用单引号
+  singleQuote: true,
+
+  // 指定代码换行的行长度
+  printWidth: 80,
+
+  // 是否使用尾逗号
+  trailingComma: 'none',
+
+  // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+  arrowParens: 'avoid',
+};
--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -1,11 +1,12 @@
-import * as fs from 'fs';
-import chalk from 'chalk';
+import * as fs from 'fs'
+import chalk from 'chalk'
 
 // 通过 GIT_PARAMS 读取到保存 git commit 时输入的描述信息的文件目录，一般路径是：.git/COMMIT_EDITMSG
 const msgPath = process.env.GIT_PARAMS
+console.log('process.env.GIT_PARAMS =', process.env.GIT_PARAMS)
 
 // 读取 .git/COMMIT_EDITMSG 文件信息
-const msg = fs.readFileSync(msgPath, 'utf-8').trim();
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
 
 /**
  * 校验提交信息格式
@@ -14,17 +15,24 @@ const msg = fs.readFileSync(msgPath, 'utf-8').trim();
  *   - 括号+冒号+空格 (括号内一般描述修改模块的名称)
  *   - 输入一些描述信息 .{1,50}
  */
-const commitRE = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert|merge)(\(.+\))?: .{1,50}/;
+const commitRE =
+  /^(revert: )?(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert|merge)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
-  console.log();
+  console.log()
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`Invalid commit message format.`)}\n\n` +
-      chalk.red(`  Proper commit message format is required for automated changelog generation.\n\n`) +
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      `Invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation.\n\n`
+      ) +
       `  ${chalk.green(`Examples: feat(compiler): add 'comments' option`)}\n` +
-      `  ${chalk.green(`  - type must be one of [feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert, merge]`)}\n` +
+      `  ${chalk.green(
+        `  - type must be one of [feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert, merge]`
+      )}\n` +
       `  ${chalk.green(`  - header must not be longer than 50 characters`)}\n` +
-      `  ${chalk.green(`  - subject may not be empty`)}\n`,
-  );
-  process.exit(1);
+      `  ${chalk.green(`  - subject may not be empty`)}\n`
+  )
+  process.exit(1)
 }
